29a9c75609365938a614054be5fa012042ba6e50
Stephan Moeller <stephanryer@hotmail.com>
2026-01-12 08:59:48 +0100

    added r to custom gaming layer

--- src/shared_keymap.zig
+++ src/shared_keymap.zig
@@ -71,10 +71,10 @@ pub const keymap = [_][key_count]core.KeyDef{
    },
     // GAMING
     .{
-           T(us.ESCAPE),    NONE,    NONE,    NONE,    NONE,                   NONE,       NONE,   T(us.UP),        NONE,    custom_key(CUSTO_TAP_DISABLE_GAMING),
+           T(us.ESCAPE),    NONE,    T(dk.R),    NONE,    NONE,                   NONE,       NONE,   T(us.UP),        NONE,    custom_key(CUSTO_TAP_DISABLE_GAMING),
            NONE, T(dk.A), T(dk.S), T(dk.T),    NONE,                   NONE, T(us.LEFT), T(us.DOWN), T(us.RIGHT),    NONE,
            T(dk.G),    T(dk.G),    NONE,    NONE,                            NONE,       NONE,       NONE,        T(us.ESCAPE),
-                                        T(us.ENTER),                  T(us.SPACE)
+                                        T(us.SPACE),                  NONE
     },
 };
 
